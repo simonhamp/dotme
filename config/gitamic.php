25b55d043dee4fd7660d6e31951792803503b040
Spock <spock@example.com>
2023-08-30 03:06:10 +0100

    Try again

--- config/gitamic.php
+++ config/gitamic.php
@@ -5,8 +5,8 @@ return [
     'use_authenticated' => false,
 
     'user' => [
-        'name' => env('GITAMIC_GIT_USER_NAME', env('STATAMIC_GIT_USER_NAME')),
-        'email' => env('GITAMIC_GIT_USER_EMAIL',  env('STATAMIC_GIT_USER_EMAIL')),
+        'name' => env('GITAMIC_GIT_USER_NAME'),
+        'email' => env('GITAMIC_GIT_USER_EMAIL'),
     ],
 
     'cache' => [
